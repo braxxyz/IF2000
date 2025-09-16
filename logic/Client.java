46bf38978f507e09d95123e98b39fab5d0680d3b
braxxyz <yenamoretty2327@gmail.com>
2025-09-16 13:33:03 -0600

    terminando el client

---
+++ logic/Client.java
@@ -0,0 +1,10 @@
+public class Client {
+
+    public class Client extends Person {
+        private String address;
+
+        public Client (String name , String lastname, String id, String phone, int age, String address) {
+            super(name, lastaname, id, phone, age )
+            
+    }
+}
